b0234fdcc63f0542224bab92f4bd535aab56d605
Joe Kasper <joe.kasper@gmail.com>
2017-06-08 07:40:21 -0700

    Additional plots of components in PCA

--- pca.py
+++ pca.py
@@ -28,3 +28,29 @@ def doPCA(data,w1,w3,tau2,n_comp=10):
         plt.setp(ax.get_yticklabels(), visible=True)
         plt.setp(ax.get_xticklabels(), visible=True)
     plt.show()
+
+    data_c = pca.transform(data_r)
+
+    # Plot against time
+    plt.figure(figsize=(5,5))
+    plt.scatter(tau2, data_c[:,0], color='red', label='C0') # component 0
+    plt.scatter(tau2, data_c[:,1], color='orange', label='C1')
+    plt.scatter(tau2, data_c[:,2], color='green', label='C2')
+    plt.scatter(tau2, data_c[:,3], color='blue', label='C3')
+    plt.xlabel('Time')
+    plt.legend(loc='lower right')
+    
+    # Plot value of component contribution vs. component for selected times
+    plt.figure(figsize=(5,5))
+    comp_num = list(range(10))
+    plt.plot(comp_num, (data_c[0]), color='red', label='t=0') # component 0
+    plt.plot(comp_num, (data_c[3]), color='orange', label='t=270')
+    plt.plot(comp_num, (data_c[6]), color='green', label='t=540')
+    plt.plot(comp_num, (data_c[9]), color='blue', label='t=5000')
+    plt.plot(comp_num, (data_c[12]), color='purple', label='t=20000')
+    plt.xlim(-0.5,10.5)
+    plt.ylim(-5, 6.6)
+    plt.xlabel('Component')
+    plt.ylabel('Component contribution')
+    plt.legend(loc='upper right')
+    plt.show()
