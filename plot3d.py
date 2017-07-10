8ea3ba33b3821765d870842e78e7e95c3c618747
Joe Kasper <jkasper2@uw.edu>
2017-07-09 17:48:02 -0700

    added a slider for 3d surfaces

--- plot3d.py
+++ plot3d.py
@@ -4,18 +4,21 @@ import matplotlib.ticker as ticker
 from matplotlib import cm
 from mpl_toolkits.mplot3d import Axes3D
 
-def surf3d(x,y,Z):
+def surf3d(x,y,Z,window_title='Figure',ax_title='',fig='None',azim=-50,elev=25):
     '''
     Performs 3d surface plot for x,y,Z data
     '''
     X,Y = np.meshgrid(y,x)
-    fig = plt.figure()
+    if(fig=='None'):
+        fig = plt.figure(figsize=(12,6))
+    fig.canvas.set_window_title(window_title)
     ax = fig.gca(projection='3d')
 
     TICK_LIMIT = 5
 
     surf = ax.plot_surface(X,Y,Z,rstride=2,cstride=2,linewidth=0,cmap=cm.rainbow)
-    ax.view_init(elev=25., azim=-50)
+    ax.set_zlim3d(-1.0,1.0)
+    ax.view_init(elev=elev, azim=azim)
     fig.colorbar(surf,shrink=0.5, aspect=5)
     ax.set_xlabel('$\omega_{1} (cm^{-1})$')
     ax.set_ylabel('$\omega_{3} (cm^{-1})$')
@@ -27,6 +30,7 @@ def surf3d(x,y,Z):
     yticks = ax.get_yticks()
     nskip = round(len(yticks)/TICK_LIMIT)
     ax.set_yticks(yticks[::nskip])
+    ax.set_title(ax_title)
 
     return ax
 
